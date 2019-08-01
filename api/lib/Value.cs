21d8d9b736f26a8731a87b4a8645db612c89cfe7
Joshua Honig <joshua@mallowfields.com>
2019-08-01 14:37:42 -0400

    Static Value API

---
+++ api/lib/Value.cs
@@ -0,0 +1,14 @@
+﻿namespace Ockham.Data
+{ 
+    public static class Value
+    { 
+        public static bool IsNumeric(object value) => throw null;
+        public static bool IsNumeric(object value, ConvertOptions options) => throw null; 
+        public static bool IsDefault(object value) => throw null;
+        public static bool IsNull(object value) => throw null;
+        public static bool IsNull(object value, bool emptyStringAsNull) => throw null;
+        public static bool IsNull(object value, ConvertOptions options) => throw null;
+        public static bool IsNullOrEmpty(object value) => throw null;
+        public static bool IsNullOrWhitespace(object value) => throw null; 
+    }
+}

35e0c82456873d77046d4e69fc79cc2096f07e0b
Joshua Honig <joshua@mallowfields.com>
2019-08-01 14:37:57 -0400

    Configurable Converter API

---
+++ api/lib/Converter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ockham.Data
+{
+    public delegate object ConverterDelegate(object value, Type targetType, ConvertOptions options);
+    public delegate object ConverterDelegate<T>(object value, ConvertOptions options);
+
+    public partial class Converter
+    {
+        public static Converter Default { get; } = new Converter(ConvertOptions.Default);
+
+        public Converter(ConvertOptions options) => throw null;
+        public Converter(ConvertOptions options, params (Type targetType, ConverterDelegate @delegate)[] converters) => throw null;
+
+        public ConvertOptions Options { get; }
+
+        public object Force(object value, Type targetType) => throw null;
+        public T Force<T>(object value) => throw null;
+        public T Force<T>(object value, T defaultValue) => throw null;
+        public T To<T>(object value) => throw null;
+        public object To(object value, Type targetType) => throw null;
+
+        public bool IsNumeric(object value) => throw null;
+        public bool IsNull(object value) => throw null;
+        public object ToNull(object value) => throw null;
+        public object ToDBNull(object value) => throw null;
+
+        public IReadOnlyDictionary<Type, ConverterDelegate> Converters { get; }
+        public Converter WithConverter<T>(ConverterDelegate<T> @delegate) => throw null;
+        public Converter WithConverter(Type targetType, ConverterDelegate @delegate) => throw null;
+        public Converter WithConverters((Type targetType, ConverterDelegate @delegate) converter, params (Type targetType, ConverterDelegate @delegate)[] converters) => throw null;
+    }
+
+    public partial class Converter
+    {
+        public bool ToBool(object value) => throw null;
+        public DateTime ToDate(object value) => throw null;
+        public decimal ToDec(object value) => throw null;
+        public double ToDbl(object value) => throw null;
+        public Guid ToGuid(object value) => throw null;
+        public int ToInt(object value) => throw null;
+        public long ToLng(object value) => throw null;
+        public string ToStr(object value) => throw null;
+        public TimeSpan ToTimeSpan(object value) => throw null;
+    }
+}

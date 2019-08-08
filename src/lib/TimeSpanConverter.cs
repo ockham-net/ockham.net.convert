b2133dfabb9e625b4e9d97423640d03057748ff7
Joshua Honig <joshua@mallowfields.com>
2019-08-08 12:51:41 -0400

    Implement TimeSpanConverter and tests

---
+++ src/lib/TimeSpanConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using VBConvert = Microsoft.VisualBasic.CompilerServices.Conversions;
+
+namespace Ockham.Data
+{
+    /// <summary>
+    /// A utility for converting values to <see cref="TimeSpan"/>
+    /// </summary>
+    public static class TimeSpanConverter
+    {
+        private const string RX_MINUTES = @"^(?<minutes>[0-9]{1,2}):[0-9]{2}(\.[0-9]+)?$";
+
+        /// <summary>
+        /// Convert the provided value to a <see cref="TimeSpan"/>
+        /// </summary>  
+        public static TimeSpan ToTimeSpan(object value, ConvertOptions options)
+        {
+            if (value is TimeSpan) return (TimeSpan)value;
+            if (value is string && Regex.IsMatch((string)value, @"^\s*\d+\.\d+\s*$"))
+            {
+                // Treat decimal string as seconds, not ticks
+                return TimeSpan.FromSeconds(Convert.To<double>(value, options));
+            }
+            else if (value != null && Value.IsNumeric(value, options))
+            {
+                return TimeSpan.FromTicks(Convert.To<long>(value, options));
+            }
+            else if (value is string stringValue)
+            {
+                var m = Regex.Match(stringValue, RX_MINUTES);
+                if (m.Success)
+                {
+                    // The input is a minutes : seconds string. The built-in TimeSpan.Parse requires a leading
+                    // hours segment as well. Append the 0: hours segment, and also pad the minutes segment with
+                    // a leading 0 if necessary
+                    stringValue = "0:" + (m.Groups["minutes"].Value.Length == 2 ? "" : "0") + stringValue;
+                }
+
+                return TimeSpan.Parse(stringValue);
+            }
+            else if (value is DateTime)
+            {
+                return TimeSpan.FromTicks(((DateTime)value).Ticks);
+            }
+
+            // If the input is not a TimeSpan, this will throw an invalid cast exception in the localized language:
+            return (TimeSpan)value;
+        }
+    }
+}
---
+++ tests/lib/Converters/TimeSpanConverterTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Xunit;
+using Ockham.Data.Tests.Fixtures;
+
+using static Ockham.Data.Tests.Factories;
+
+namespace Ockham.Data.Tests
+{
+    [ExcludeFromCodeCoverage]
+    public class TimeSpanConverterTests
+    {
+        public static IEnumerable<object[]> Unconvertible => (new object[]
+        {
+            "Hello",
+            new object(),
+            (Func<int>)(() => 42)
+        }).AsObjectArray();
+
+        public static IEnumerable<object[]> ComplexStrings => new[]
+        {
+            Pair("0:02.231", new TimeSpan(0, 0, 0, 2, 231)),
+            Pair("5:14", new TimeSpan(0, 0, 5, 14, 0)),
+            Pair("5:14.129", new TimeSpan(0, 0, 5, 14, 129)),
+            Pair("01:00.010", new TimeSpan(0, 0, 1, 0, 10)),
+            Pair("3:12:00.000", new TimeSpan(0, 3, 12, 0, 0)),
+            Pair("231.3:12:00.000", new TimeSpan(231, 3, 12, 0, 0))
+        };
+
+        public static IEnumerable<object[]> Timespans => ComplexStrings.Select(arr => arr[1]).AsObjectArray();
+
+        [Theory]
+        [MemberData(nameof(Timespans))]
+        public void PreserveTimeSpan(TimeSpan input)
+        {
+            Assert.Equal(input, TimeSpanConverter.ToTimeSpan(input, ConvertOptions.Default));
+        }
+
+        [Theory]
+        [MemberData(nameof(TestPrimitives.DecimalStrings), MemberType = typeof(TestPrimitives))]
+        public void DecimalSecondsToTimespan(object input)
+        {
+            double dblVal = Convert.To<double>(input);
+            Assert.Equal(TimeSpan.FromSeconds(dblVal), TimeSpanConverter.ToTimeSpan(input, ConvertOptions.Default));
+        }
+
+        [Fact]
+        public void DateTimeToTimespan()
+        {
+            DateTime dateTime = DateTime.UtcNow;
+            Assert.Equal(TimeSpan.FromTicks(dateTime.Ticks), TimeSpanConverter.ToTimeSpan(dateTime, ConvertOptions.Default));
+        }
+
+        [Theory]
+        [MemberData(nameof(TestPrimitives.WholeNumeric), MemberType = typeof(TestPrimitives))]
+        public void TicksToTimespan(object input)
+        {
+            long lngVal = Convert.To<long>(input);
+            Assert.Equal(TimeSpan.FromTicks(lngVal), TimeSpanConverter.ToTimeSpan(input, ConvertOptions.Default));
+        }
+
+        [Theory]
+        [MemberData(nameof(ComplexStrings))]
+        public void TimestringToTimespan(object input, TimeSpan expected)
+        {
+            Assert.Equal(expected, TimeSpanConverter.ToTimeSpan(input, ConvertOptions.Default));
+        }
+
+        [Theory]
+        [MemberData(nameof(Unconvertible))]
+        public void GarbageInThrows(object input)
+        {
+            Assert.ThrowsAny<SystemException>(() => TimeSpanConverter.ToTimeSpan(input, ConvertOptions.Default));
+        }
+    }
+}

fcd1d30f9a96a530a8c769c4278c9e490cf4c067
Joshua Honig <joshua@mallowfields.com>
2019-08-08 12:35:53 -0400

    Test conversion to enum

---
+++ tests/lib/Convert/Convert.To.Enum.cs
@@ -0,0 +1,45 @@
+﻿using Ockham.Data.Tests.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+using static Ockham.Data.Tests.Factories;
+
+namespace Ockham.Data.Tests
+{
+    public partial class ConvertTests
+    {
+
+        public enum OtherEnum
+        {
+            NineAndForty = 49
+        }
+
+        public static IEnumerable<object[]> ConvertToEnumData = Sets<object, ConvertOptions, TestShortEnum>(
+            (49, ConvertOptions.Default, TestShortEnum.FortyNine),
+            ("49", ConvertOptions.Default, TestShortEnum.FortyNine),
+            ("FortyNine", ConvertOptions.Default, TestShortEnum.FortyNine),
+            (OtherEnum.NineAndForty, ConvertOptions.Default, TestShortEnum.FortyNine),
+            (new ComplexNumber(49, 0), ConvertOptions.Default, TestShortEnum.FortyNine),
+            (null, OptionsVariant.NullToValueDefault, TestShortEnum.Zero),
+            (DBNull.Value, OptionsVariant.NullToValueDefault, TestShortEnum.Zero),
+            ("0x31", OptionsVariant.ParseBaseN, TestShortEnum.FortyNine),
+            ("0O61", OptionsVariant.ParseBaseN, TestShortEnum.FortyNine),
+            ("0b110001", OptionsVariant.ParseBaseN, TestShortEnum.FortyNine) 
+        );
+
+        [Theory]
+        [MemberData(nameof(ConvertToEnumData))]
+        public static void ConvertToEnum(object value, ConvertOptions options, TestShortEnum expected)
+        {
+            TestOverloads<TestShortEnum>(null, true, value, options, (opts, invoke) =>
+            {
+                var result = invoke();
+                Assert.IsType<TestShortEnum>(result);
+                Assert.Equal(expected, (TestShortEnum)result);
+            });
+        }
+    }
+}

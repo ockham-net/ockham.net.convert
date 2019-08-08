7fa245de21a9c45fbb1004d2619f824952c89f56
Joshua Honig <joshua@mallowfields.com>
2019-08-08 12:19:07 -0400

    Test convert to reference types

---
+++ tests/lib/Convert/Convert.To.Reference.cs
@@ -0,0 +1,56 @@
+﻿using Ockham.Data.Tests.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+using static Ockham.Data.Tests.Factories;
+
+namespace Ockham.Data.Tests
+{
+    public partial class ConvertTests
+    {
+        public static IEnumerable<object[]> StarData = Values("hi", 2, new object());
+
+        public static IEnumerable<object[]> StringData = Sets(
+            Pair(123, "123"),
+            Pair(3.3333m, "3.3333"),
+            Pair(new NeutronStar(), "I'm a neutron star")
+        );
+
+        // Custom converter is invoked
+        [Theory]
+        [MemberData(nameof(StarData))]
+        public void ToCustomClass(object value)
+        {
+            long invokeCount = 0;
+            var star = new Star();
+            var options = ConvertOptionsBuilder.Default
+                .WithConverter<Star>((_, opts) =>
+                {
+                    invokeCount++;
+                    return star;
+                }).Options;
+
+            TestOverloads<Star>(null, true, value, options, (opts, invoke) =>
+            {
+                object result = null;
+                ConvertAssert.Increments(ref invokeCount, () => result = invoke());
+                Assert.Same(result, star);
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(StringData))]
+        public void ConvertToString(object value, string expected)
+        {
+            TestOverloads<string>(value, ConvertOptions.Default, (options, invoke) =>
+            {
+                var result = invoke() as string;
+                Assert.Equal(expected, result);
+            });
+        }
+
+    }
+}
---
+++ tests/lib/Utilities/Factories.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Ockham.Data.Tests
+{
+    [ExcludeFromCodeCoverage]
+    public static class Factories
+    {
+        public static object[] Pair(object item1, object item2)
+        {
+            return new object[] { item1, item2 };
+        }
+
+        public static IEnumerable<object[]> Values(params object[] values) => values.AsObjectArray();
+
+        public static IEnumerable<object[]> Sets(params object[][] values) => values;
+
+        public static IEnumerable<Func<object>> Funcs(params Func<object>[] tests) => tests;
+
+
+        public static IEnumerable<object[]> Sets<T1, T2>(params (T1, T2)[] values) => values.Select(o => new object[] { o.Item1, o.Item2 });
+        public static IEnumerable<object[]> Sets<T1, T2, T3>(params (T1, T2, T3)[] values) => values.Select(o => new object[] { o.Item1, o.Item2, o.Item3 });
+        public static IEnumerable<object[]> Sets<T1, T2, T3, T4>(params (T1, T2, T3, T4)[] values) => values.Select(o => new object[] { o.Item1, o.Item2, o.Item3, o.Item4 });
+
+
+        public static object[] Set(params object[] values) => values;
+
+        public static object[] Set<T1, T2, T3, T4>(T1 arg1, T2 arg2, T3 arg3, T4 arg4) => new object[] { arg1, arg2, arg3, arg4 };
+    }
+}

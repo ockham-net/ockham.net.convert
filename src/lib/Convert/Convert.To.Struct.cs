0e978c82cbc2810f8a387c5682e176e11b482d45
Joshua Honig <joshua@mallowfields.com>
2019-08-08 12:34:00 -0400

    Test instrumentation and performance critical paths

---
+++ src/lib/Convert/Convert.To.Struct.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Ockham.Data
+{
+    public static partial class Convert
+    {
+
+        /// <summary>
+        /// This method avoids boxing or lifting value types, but requires compile-time knowledge of target type. 
+        /// </summary> 
+        internal static T ToStruct<T>(object value, ConvertOptions options, bool ignoreError, T? valueOnError) where T : struct
+        {
+            // Do NOT check for same type here. That should already be done by calling functions
+
+            if (
+#if INSTRUMENT
+                // Technically we're incrementing before the null check...but it has the desired effect
+                ((Interlocked.Increment(ref options.CountPastNullRef) > 0) && (null == value))
+#else
+                (null == value)
+#endif
+                || Value.IsNull(value, options))
+            {
+                if (options.NullToValueDefault)
+                {
+                    return default;
+                }
+                else if (ignoreError)
+                {
+                    return valueOnError ?? default;
+                }
+                else
+                {
+                    throw new InvalidCastException($"Cannot convert empty value {FormatValue(value)} to value type {FormatType(typeof(T))}");
+                }
+            }
+
+#if INSTRUMENT
+            // Allowing horrible inspection if *implementation* because performance here is so important
+            Interlocked.Increment(ref options.CountPastEmptyValue);
+#endif
+
+            return (T)ToStructValue(value, typeof(T), options, ignoreError, valueOnError);
+        }
+
+        /*
+       internal static T? ToNullable<T>(object value, ConvertOptions options, bool ignoreError, T? valueOnError, IReadOnlyDictionary<Type, ConverterDelegate> converters) where T : struct
+       {
+           if (value == null) return null;
+
+#if INSTRUMENT
+           // Allowing horrible inspection if *implementation* because performance here is so important
+           Interlocked.Increment(ref options.CountPastNullRef);
+#endif
+
+           // Do NOT check for same type here. That should already be done by calling functions
+
+           if (Value.IsNull(value, options)) return null;
+
+#if INSTRUMENT
+           Interlocked.Increment(ref options.CountPastEmptyValue);
+#endif
+
+           return (T)ToStructValue(value, typeof(T), options, ignoreError, valueOnError, converters);
+       }
+       */
+
+
+    }
+}
---
+++ tests/lib/Fixtures/TestEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ockham.Data.Tests.Fixtures
+{
+    public enum TestShortEnum : short
+    {
+        Zero = 0,
+        One = 1,
+        Two = 2,
+        FortyNine = 49
+    }
+}

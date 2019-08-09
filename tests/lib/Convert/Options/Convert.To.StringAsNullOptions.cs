0bd69882778dda7e7dd2fd8a8cf8eba61f9f00ec
Joshua Honig <joshua@mallowfields.com>
2019-08-09 08:27:09 -0400

    Test string options

---
+++ tests/lib/Convert/Options/Convert.To.StringAsNullOptions.cs
@@ -0,0 +1,96 @@
+﻿using Ockham.Data.Tests.Fixtures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace Ockham.Data.Tests
+{
+    using static ConvertTestRunner;
+
+    public partial class StringAsNullTests
+    {
+        [Fact]
+        public void NullToNull()
+        {
+            foreach (var options in new[] { ConvertOptions.Default, OptionsVariant.EmptyStringAsNull, OptionsVariant.WhitespaceAsNull })
+            {
+                TestOverloads<CelestialBody>(null, options, (opts, invoke) =>
+                {
+                    Assert.Null(invoke());
+                });
+            }
+        }
+
+        [Fact]
+        public void DBNullToNull()
+        {
+            foreach (var options in new[] { ConvertOptions.Default, OptionsVariant.EmptyStringAsNull, OptionsVariant.WhitespaceAsNull })
+            {
+                TestOverloads<CelestialBody>(DBNull.Value, options, (opts, invoke) =>
+                {
+                    Assert.Null(invoke());
+                });
+            }
+        }
+
+        [Fact]
+        public void EmptyToNull()
+        {
+            foreach (var options in new[] { ConvertOptions.Default, OptionsVariant.EmptyStringAsNull, OptionsVariant.WhitespaceAsNull })
+            {
+                // Force always converts to null
+                TestOverloads<CelestialBody>(ConvertOverload.Force, "", options, (opts, invoke) =>
+                {
+                    Assert.Null(invoke());
+                });
+            }
+
+            foreach (var options in new[] { ConvertOptions.Default })
+            {
+                // To fails because empty string is not considered null
+                TestCustomOverloads<CelestialBody>(ConvertOverload.To, true, "", options, (opts, invoke) =>
+                {
+                    Assert.ThrowsAny<SystemException>(() => invoke());
+                });
+            }
+
+            foreach (var options in new[] { OptionsVariant.EmptyStringAsNull, OptionsVariant.WhitespaceAsNull })
+            {
+                TestCustomOverloads<CelestialBody>(ConvertOverload.To, true, "", options, (opts, invoke) =>
+                {
+                    Assert.Null(invoke());
+                });
+            }
+        }
+
+        [Fact]
+        public void WhitespaceToNull()
+        {
+            string whitespace = "  \r\n  \t";
+            foreach (var options in new[] { ConvertOptions.Default, OptionsVariant.EmptyStringAsNull })
+            {
+                // Force always converts to null
+                TestOverloads<CelestialBody>(ConvertOverload.Force, whitespace, options, (opts, invoke) =>
+                {
+                    Assert.Null(invoke());
+                });
+
+                // To fails because whitespace string is not considered null
+                TestCustomOverloads<CelestialBody>(ConvertOverload.To, true, whitespace, options, (opts, invoke) =>
+                {
+                    Assert.ThrowsAny<SystemException>(() => invoke());
+                });
+            }
+
+            foreach (var options in new[] { OptionsVariant.WhitespaceAsNull })
+            {
+                TestCustomOverloads<CelestialBody>(ConvertOverload.To, true, whitespace, options, (opts, invoke) =>
+                {
+                    Assert.Null(invoke());
+                });
+            }
+        }
+    }
+}
---
+++ tests/lib/Convert/Options/Convert.To.TrimStringOptions.cs
@@ -0,0 +1,121 @@
+﻿using Ockham.Data.Tests.Fixtures;
+using System;
+using System.Reflection;
+using Xunit;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Ockham.Data.Tests
+{
+    using static ConvertTestRunner;
+
+    // Test that ConvertOptions.Enums settings have the intended effect
+    public partial class ConvertFromStringTests
+    {
+        public static readonly IEnumerable<object[]> StartPadded42s = ConvertToNumberTests.String42s_Strict.Select(o => " \r " + (o[0] as string)).AsObjectArray().ToArray();
+        public static readonly IEnumerable<object[]> EndPadded42s = ConvertToNumberTests.String42s_Strict.Select(o => (o[0] as string) + " \t ").AsObjectArray().ToArray();
+        public static readonly IEnumerable<object[]> BothPadded42s = ConvertToNumberTests.String42s_Strict.Select(o => " \r " + (o[0] as string) + " \t ").AsObjectArray().ToArray();
+        public static readonly IEnumerable<object[]> AllPadded42s = StartPadded42s.Concat(EndPadded42s).Concat(BothPadded42s).ToArray();
+
+        [Theory]
+        [MemberData(nameof(StartPadded42s))]
+        public static void AllowStartPadding_Numbers(string value)
+        {
+            var options = ConvertOptions.Default.GetBuilder()
+                .WithStringOptions(StringAsNullOption.NullReference, TrimStringFlags.TrimStart).Options;
+
+            TestCustomOverloads<int>(value, options, convert =>
+            {
+                var result = convert();
+                Assert.IsType<int>(result);
+                Assert.Equal(42, (int)result);
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(EndPadded42s))]
+        public static void AllowEndPadding_Numbers(string value)
+        {
+            var options = ConvertOptions.Default.GetBuilder()
+                .WithStringOptions(StringAsNullOption.NullReference, TrimStringFlags.TrimEnd).Options;
+
+            TestCustomOverloads<int>(value, options, convert =>
+            {
+                var result = convert();
+                Assert.IsType<int>(result);
+                Assert.Equal(42, (int)result);
+            });
+        }
+
+        [Theory]
+        [MemberData(nameof(AllPadded42s))]
+        public static void AllowBothPadding_Numbers(string value)
+        {
+            var options = ConvertOptions.Default.GetBuilder()
+                .WithStringOptions(StringAsNullOption.NullReference, TrimStringFlags.TrimAll).Options;
+
+            TestCustomOverloads<int>(value, options, convert =>
+            {
+                var result = convert();
+                Assert.IsType<int>(result);
+                Assert.Equal(42, (int)result);
+            });
+        }
+
+
+        [Theory]
+        [MemberData(nameof(StartPadded42s))]
+        public static void RejectStartPadding_Numbers_None(string value)
+        {
+            var options = ConvertOptions.Default.GetBuilder()
+                .WithStringOptions(StringAsNullOption.NullReference, TrimStringFlags.None).Options;
+
+            TestCustomOverloads<int>(ConvertOverload.To, value, options, convert =>
+            {
+                Assert.ThrowsAny<SystemException>(() => convert());
+            });
+        }
+
+
+        [Theory]
+        [MemberData(nameof(StartPadded42s))]
+        public static void RejectStartPadding_Numbers_End(string value)
+        {
+            var options = ConvertOptions.Default.GetBuilder()
+                .WithStringOptions(StringAsNullOption.NullReference, TrimStringFlags.TrimEnd).Options;
+
+            TestCustomOverloads<int>(ConvertOverload.To, value, options, convert =>
+            {
+                Assert.ThrowsAny<SystemException>(() => convert());
+            });
+        }
+
+
+        [Theory]
+        [MemberData(nameof(EndPadded42s))]
+        public static void RejectEndPadding_Numbers_None(string value)
+        {
+            var options = ConvertOptions.Default.GetBuilder()
+                .WithStringOptions(StringAsNullOption.NullReference, TrimStringFlags.None).Options;
+
+            TestCustomOverloads<int>(ConvertOverload.To, value, options, convert =>
+            {
+                Assert.ThrowsAny<SystemException>(() => convert());
+            });
+        }
+
+
+        [Theory]
+        [MemberData(nameof(EndPadded42s))]
+        public static void RejectEndPadding_Numbers_Start(string value)
+        {
+            var options = ConvertOptions.Default.GetBuilder()
+                .WithStringOptions(StringAsNullOption.NullReference, TrimStringFlags.TrimStart).Options;
+
+            TestCustomOverloads<int>(ConvertOverload.To, value, options, convert =>
+            {
+                Assert.ThrowsAny<SystemException>(() => convert());
+            });
+        }
+    }
+}
